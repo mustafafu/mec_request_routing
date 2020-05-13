87f20f9144a6b0efad14ee4d337df38477c45f37
mustafafu <furkanozkoc@gmail.com>
2020-05-12 22:57:30 -0400

    HPC compatible

--- common_script/Greedy2/auto_greedy.py
+++ common_script/Greedy2/auto_greedy.py
@@ -20,9 +20,17 @@ def fix_one(filename,modepath):
     skipcount = 0
     for idx, line in enumerate(data):
         a = line.split()
-        if idx < 5 or len(a) != 10:
-            skipcount += 1
+        did_i_find_dotdot = 0
+        if a[0] == ':':
+            did_i_find_dotdot=1
+
+        if did_i_find_dotdot==0:
+            skipcount +=1
             continue
+
+        if a[0] == ';':
+            break
+
         access = a
         for col, access_indicator in enumerate(access):
             if col == 0:
@@ -46,9 +54,17 @@ def fix_zero(filename,modepath):
     user_sum = 0
     for idx, line in enumerate(data):
         a = line.split()
-        if idx < 5 or len(a) != 10:
-            skipcount += 1
+        did_i_find_dotdot = 0
+        if a[0] == ':':
+            did_i_find_dotdot=1
+
+        if did_i_find_dotdot==0:
+            skipcount +=1
             continue
+
+        if a[0] == ';':
+            break
+
         access = a
         for col, access_indicator in enumerate(access):
             if col == 0:
@@ -139,8 +155,8 @@ if __name__ == '__main__':
     #                        zero_constraint=args.zero_constraint, threshold=args.threshold,
     #                         filename='./Output/greedy_output.txt', modepath='./semi_mip.mod')
     scenario_number = sys.argv[4]
-    modepath_string = './semi_mip_' + str(scenario_number) + '.mod'
-    filename_string = './Output/greedy_output_' + str(scenario_number) + '.txt'
+    modepath_string = './iter_models/semi_mip_' + str(scenario_number) + '.mod'
+    filename_string = '../Output/greedy_output_' + str(scenario_number) + '.txt'
     flag = get_max_from_lp(filename=filename_string,modepath=modepath_string,iter=sys.argv[1],zero_constraint=sys.argv[2],threshold=sys.argv[3])
 
 #     print(max_value,position)
