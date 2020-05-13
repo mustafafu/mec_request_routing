05de903a9d7c81d9edcefb24a4764b7fdef2acf2
chenli1996 <63168218+chenli1996@users.noreply.github.com>
2020-05-12 21:08:03 -0400

    update

--- common_script/auto.sh
+++ common_script/auto.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 echo "include semi_mip.run;exit;" | ../ampl_linux-intel64/ampl > ./Output/greedy_output.txt
-#flag=$(python3 ./auto_greedy.py 0)
-flag=$(python3 auto_greedy.py 0 2>&1 > /dev/null)
-iteration=1
-#zero="0"
-#while ["$flag" != "$zero"]
+
+
+threshold=0.85
+zero_constraint=0
+echo $flag
+iteration=0
+
 while [ "$flag" != "stop" ]
 do
 	echo "include semi_mip.run;exit;" | ../ampl_linux-intel64/ampl > ./Output/greedy_output.txt
-	flag=$(python3 auto_greedy.py $iteration 2>&1 > /dev/null)
-#	$(python3 ./auto_greedy.py $iteration)
+	flag=$(python3 auto_greedy.py $iteration $zero_constraint $threshold 2>&1 > /dev/null)
+	len=${#flag}
+	if (($len > 6));
+	then
+		zero_constraint=$((zero_constraint+1))
+	else
+		zero_constraint=0
+	fi
 	iteration=$((iteration+1))
 	echo $flag
-#	echo "include lp.run;exit;" | ../ampl_linux-intel64/ampl > ./Output/greedy_output.txt
-done
 
+done
+#cp ./semi_mip_backup.mod ./semi_mip.mod
--- common_script/auto_greedy.py
+++ common_script/auto_greedy.py
@@ -1,11 +1,88 @@
 import sys
-def get_max_from_lp(filename='./Output/greedy_output.txt',modepath='./semi_mip.mod',iter = 1):
+#
+# import argparse
+#
+# parser = argparse.ArgumentParser()
+# parser.add_argument('iter',type=int,default=2)
+# parser.add_argument('zero_constraint',type=int,default=5)
+# parser.add_argument('threshold',type=float,default=0.8)
+# args = parser.parse_args()
+
+# iter=sys.argv[1],zero_constraint=sys.argv[2],threshold=sys.argv[3]
+
+def fix_one(filename,modepath):
+    fp2 = open(filename, 'r')
+    fp_fixone = open(modepath,'a')
+    data = fp2.readlines()
+    max_value = 0
+    position = [-1, -1]
+    flag = 0
+    skipcount = 0
+    for idx, line in enumerate(data):
+        a = line.split()
+        if idx < 5 or len(a) != 10:
+            skipcount += 1
+            continue
+        access = a
+        for col, access_indicator in enumerate(access):
+            if col == 0:
+                continue
+            access_indicator = float(access_indicator)
+            if access_indicator == 1:
+                position = [idx - skipcount + 1, col]
+                fp_fixone.write('s.t. C_fixone' + str(position[0])+ str(position[1]) + ': access_indicator[' + str(position[1])
+                                + ',' + str(position[0]) + '] = 1;\n')
+    fp2.close()
+    fp_fixone.close()
+
+def fix_zero(filename,modepath):
+    fp2 = open(filename, 'r')
+    fp_fixzero = open(modepath,'a')
+    data = fp2.readlines()
+    max_value = 0
+    position = [-1, -1]
+    flag = 0
+    skipcount = 0
+    user_sum = 0
+    for idx, line in enumerate(data):
+        a = line.split()
+        if idx < 5 or len(a) != 10:
+            skipcount += 1
+            continue
+        access = a
+        for col, access_indicator in enumerate(access):
+            if col == 0:
+                continue
+            access_indicator = float(access_indicator)
+            user_sum += access_indicator
+        #print(user_sum)
+        if user_sum < 1:
+         #   print('****')
+
+            position = [idx - skipcount + 1, col]
+            fp_fixzero.write(
+                's.t. C_fixzero' + str(position[0])+ str(position[1]) + ' : sum {n in Nodes} access_indicator[n,' + str(
+                    position[0]) + '] = 0;\n')
+        user_sum = 0
+    fp2.close()
+    fp_fixzero.close()
+
+def get_max_from_lp(filename='./Output/greedy_output.txt',modepath='./semi_mip.mod',iter = 1,zero_constraint = 5,threshold = 0.8):
+    threshold = float(threshold)
+    iter=str(iter)
+    # zero_constraint+=1
+    # print(zero_constraint)
     fp = open(filename,'r')
     data = fp.readlines()
     max_value = 0
     position = [-1,-1]
     flag = 0
     skipcount = 0
+    if iter == '0':
+        fix_one(filename, modepath)
+    if zero_constraint == '2':
+        fix_zero(filename, modepath)
+        return 'stop'
     for idx,line in enumerate(data):
         a = line.split()
         if idx<5 or len(a)!=10:
@@ -16,6 +93,7 @@ def get_max_from_lp(filename='./Output/greedy_output.txt',modepath='./semi_mip.m
             if col == 0:
                 continue
             access_indicator = float(access_indicator)
+
             if access_indicator !=1 and access_indicator> max_value:
                 max_value = access_indicator
                 position = [idx-skipcount+1,col]
@@ -34,11 +112,14 @@ def get_max_from_lp(filename='./Output/greedy_output.txt',modepath='./semi_mip.m
 
 
         fp1 = open(modepath,'a')
-        if sum(sum_u[1:])<0.98:
-            fp1.write('s.t. C' + str(iter) + ': access_indicator[' + str(position[1]) + ',' + str(
-                position[0]) + '] = 0;\n')
+        if sum(sum_u[1:])<threshold:
+            # fp1.write('s.t. C' + str(iter) + ': access_indicator[' + str(position[1]) + ',' + str(
+            #     position[0]) + '] = 0;\n')
             # for i in range(9):
             #     fp1.write('s.t. C' + str(iter)+'_'+str(i+1) + ': access_indicator[' + str(i+1) + ',' + str(position[0]) + '] = 0;\n')
+            fp1.write('s.t. access_constraint_lowerbound'+str(iter)+' : sum {n in Nodes} access_indicator[n,'+str(position[0])+'] = 0;\n')
+            max_value = 'sum'+str(sum_u[1:])+'max '+str(max_value)
+
         else:
             fp1.write('s.t. C'+str(iter)+': access_indicator['+str(position[1])+','+str(position[0])+'] = 1;\n')
         fp1.close()
@@ -46,8 +127,10 @@ def get_max_from_lp(filename='./Output/greedy_output.txt',modepath='./semi_mip.m
     return max_value#return 0 means done
 
 if __name__ == '__main__':
-
-    flag = get_max_from_lp(iter = sys.argv[1])
+    # flag = get_max_from_lp( iter=args.iter,
+    #                        zero_constraint=args.zero_constraint, threshold=args.threshold,
+    #                         filename='./Output/greedy_output.txt', modepath='./semi_mip.mod')
+    flag = get_max_from_lp(filename='./Output/greedy_output.txt',modepath='./semi_mip.mod',iter=sys.argv[1],zero_constraint=sys.argv[2],threshold=sys.argv[3])
 
 #     print(max_value,position)
     exit(flag)
