e3b66d4551ef5be216f3734c70fb8ccff8cef46d
mustafafu <33717663+mustafafu@users.noreply.github.com>
2020-05-12 21:45:35 -0400

    made hpc compatible

--- common_script/auto.sh
+++ common_script/auto.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-echo "include semi_mip.run;exit;" | ../ampl_linux-intel64/ampl > ./Output/greedy_output.txt
+iter=$1
+
+cp ./semi_mip_backup.mod ./semi_mip_${iter}.mod
+
+greedy2_start=`date +%s%3N`
+
+
+echo "reset;
+model semi_mip_${iter}.mod;
+data './Data/service_${iter}.dat';
+option solver '../ampl_linux-intel64/cplex';
+solve;
+option display_round 2;
+display access_indicator;
+display revenue;" > semi_mip_${iter}.run
+
+echo "include semi_mip_${iter}.run;exit;" | ../ampl_linux-intel64/ampl > ./Output/greedy_output_${iter}.txt
+
 
 
 threshold=0.85
@@ -9,8 +26,8 @@ iteration=0
 
 while [ "$flag" != "stop" ]
 do
-	echo "include semi_mip.run;exit;" | ../ampl_linux-intel64/ampl > ./Output/greedy_output.txt
-	flag=$(python3 auto_greedy.py $iteration $zero_constraint $threshold 2>&1 > /dev/null)
+	echo "include semi_mip_${iter}.run;exit;" | ../ampl_linux-intel64/ampl > ./Output/greedy_output_${iter}.txt
+	flag=$(python3 auto_greedy.py $iteration $zero_constraint $threshold $iter 2>&1 > /dev/null)
 	len=${#flag}
 	if (($len > 6));
 	then
@@ -22,4 +39,9 @@ do
 	echo $flag
 
 done
-#cp ./semi_mip_backup.mod ./semi_mip.mod
+
+
+greedy2_end=`date +%s%3N`
+echo "Time to run $((greedy2_end-greedy2_start))"
+
+
